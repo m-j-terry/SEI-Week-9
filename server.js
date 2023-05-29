78fd670542efae9720aa13c905e1072b112fc413
m-j-terry <michaeljterry0815@gmail.com>
2023-05-29 00:45:50 -0400

    magic 8 ball

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.get('/tip/:total/:tipPercentage', async (req, res) => {
     res.send(`The tip should be ${tip}!`)
 })
 
+//MAGIC 8 BALL
+app.get('/magic/:question', async (req, res) => {
+    const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let question = req.params.question
+    let response = Math.floor(Math.random() * eightBall.length) + 1
+    res.send(`${question}? Magic 8 Ball says... ${eightBall[response]}!`)
+})
+
 app.listen(3000, () => {
     console.log('I\'m listening!')
 })
